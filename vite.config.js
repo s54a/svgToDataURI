4680e55f807d972f322b8be7fde33622283ab5ed
Sooraj Gupta <soorajgupta00@gmail.com>
2026-01-13 20:05:39 +0530

    routing fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
@@ -11,5 +12,14 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        chatgpt: resolve(__dirname, "chatgpt.html"),
+        claude: resolve(__dirname, "claude.html"),
+      },
+    },
   },
 });
